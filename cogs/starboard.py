daaa8e3fbffcf79c435008ed9aca0b3839138f96
Pika <15848969+ThatNerdyPikachu@users.noreply.github.com>
2018-02-23 18:04:30 -0500

    changes starboard to use raw events

--- cogs/starboard.py
+++ cogs/starboard.py
@@ -5,70 +5,74 @@ class Starboard:
     def __init__(self, bot):
         self.bot = bot
 
-    async def on_reaction_add(self, reaction, member):
-        db = dataset.connect("sqlite:///{}.db".format(reaction.message.guild.id))
+    async def on_raw_reaction_add(self, emoji, message, channel, user):
+        channel = self.bot.get_channel(channel)
+        message = await channel.get_message(message)
+        db = dataset.connect("sqlite:///{}.db".format(message.guild.id))
         table = db["starboard"]
         config = db["config"]
-        if reaction.emoji == '⭐':
-            if not table.find_one(message=reaction.message.id):
+        if emoji.name == '⭐':
+            if not table.find_one(message=message.id):
                 if config.find_one(key="starboard_channel") is None or config.find_one(key="starboard_channel")["value"] == "starboard":
-                    channel = discord.utils.get(reaction.message.guild.channels, name="starboard")
+                    channel = discord.utils.get(message.guild.channels, name="starboard")
                 else:
-                    channel = discord.utils.get(reaction.message.guild.channels, name=config.find_one(key="starboard_channel")["value"])
-                if reaction.message.channel == channel:
+                    channel = discord.utils.get(message.guild.channels, name=config.find_one(key="starboard_channel")["value"])
+                if message.channel == channel:
                     return
-                embed = discord.Embed(title="Message by {} starred".format(reaction.message.author.name), description="{}".format(reaction.message.content), color=0xffff00)
+                embed = discord.Embed(title="Message by {} starred".format(message.author.name), description="{}".format(message.content), color=0xffff00)
                 embed.set_footer(text="This message has received 1 ⭐")
-                if reaction.message.attachments:
-                    embed.set_image(url=reaction.message.attachments[0].url)
+                if message.attachments:
+                    embed.set_image(url=message.attachments[0].url)
                 msg = await channel.send(embed=embed)
-                table.insert(dict(message=reaction.message.id, stars=1, starmessage=msg.id))
+                table.insert(dict(message=message.id, stars=1, starmessage=msg.id))
             else:
                 if config.find_one(key="starboard_channel") is None or config.find_one(key="starboard_channel")["value"] == "starboard":
-                    channel = discord.utils.get(reaction.message.guild.channels, name="starboard")
+                    channel = discord.utils.get(message.guild.channels, name="starboard")
                 else:
-                    channel = discord.utils.get(reaction.message.guild.channels, name=config.find_one(key="starboard_channel")["value"])
-                data = table.find_one(message=reaction.message.id)
+                    channel = discord.utils.get(message.guild.channels, name=config.find_one(key="starboard_channel")["value"])
+                data = table.find_one(message=message.id)
                 stars = data["stars"] + 1
-                table.update(dict(message=reaction.message.id, stars=stars), ["message"])
-                data = table.find_one(message=reaction.message.id)
+                table.update(dict(message=message.id, stars=stars), ["message"])
+                data = table.find_one(message=message.id)
                 msg = discord.utils.get(await channel.history().flatten(), id=data["starmessage"])
-                embed = discord.Embed(title="Message by {} starred".format(reaction.message.author.name), description="{}".format(reaction.message.content), color=0xffff00)
+                embed = discord.Embed(title="Message by {} starred".format(message.author.name), description="{}".format(message.content), color=0xffff00)
                 embed.set_footer(text="This message has received {} ⭐'s".format(data["stars"]))
-                if reaction.message.attachments:
-                    embed.set_image(url=reaction.message.attachments[0].url)
+                if message.attachments:
+                    embed.set_image(url=message.attachments[0].url)
                 await msg.edit(embed=embed)
 
-    async def on_reaction_remove(self, reaction, member):
-        db = dataset.connect("sqlite:///{}.db".format(reaction.message.guild.id))
+    async def on_raw_reaction_remove(self, emoji, message, channel, user):
+        channel = self.bot.get_channel(channel)
+        message = await channel.get_message(message)
+        db = dataset.connect("sqlite:///{}.db".format(message.guild.id))
         table = db["starboard"]
         config = db["config"]
-        if reaction.emoji == '⭐':
-            if table.find_one(message=reaction.message.id):
-                data = table.find_one(message=reaction.message.id)
+        if emoji.name == '⭐':
+            if table.find_one(message=message.id):
+                data = table.find_one(message=message.id)
                 if config.find_one(key="starboard_channel") is None or config.find_one(key="starboard_channel")["value"] == "starboard":
-                    channel = discord.utils.get(reaction.message.guild.channels, name="starboard")
+                    channel = discord.utils.get(message.guild.channels, name="starboard")
                 else:
-                    channel = discord.utils.get(reaction.message.guild.channels, name=config.find_one(key="starboard_channel")["value"])
+                    channel = discord.utils.get(message.guild.channels, name=config.find_one(key="starboard_channel")["value"])
                 msg = discord.utils.get(await channel.history().flatten(), id=data["starmessage"])
                 stars = data["stars"] - 1
                 if stars == 0:
                     await msg.delete()
-                    table.delete(message=reaction.message.id)
+                    table.delete(message=message.id)
                     return
-                table.update(dict(message=reaction.message.id, stars=stars), ["message"])
-                data = table.find_one(message=reaction.message.id)
+                table.update(dict(message=message.id, stars=stars), ["message"])
+                data = table.find_one(message=message.id)
                 if stars == 1:
-                    embed = discord.Embed(title="Message by {} starred".format(reaction.message.author.name), description="{}".format(reaction.message.content), color=0xffff00)
+                    embed = discord.Embed(title="Message by {} starred".format(message.author.name), description="{}".format(message.content), color=0xffff00)
                     embed.set_footer(text="This message has received 1 ⭐")
-                    if reaction.message.attachments:
-                        embed.set_image(url=reaction.message.attachments[0].url)
+                    if message.attachments:
+                        embed.set_image(url=message.attachments[0].url)
                     await msg.edit(embed=embed)
                 else:
-                    embed = discord.Embed(title="Message by {} starred".format(reaction.message.author.name), description="{}".format(reaction.message.content), color=0xffff00)
+                    embed = discord.Embed(title="Message by {} starred".format(reaction.message.author.name), description="{}".format(message.content), color=0xffff00)
                     embed.set_footer(text="This message has received {} ⭐'s".format(data["stars"]))
-                    if reaction.message.attachments:
-                        embed.set_image(url=reaction.message.attachments[0].url)
+                    if message.attachments:
+                        embed.set_image(url=message.attachments[0].url)
                     await msg.edit(embed=embed)
 
 def setup(bot):
