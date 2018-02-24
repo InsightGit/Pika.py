99b2d3898b37dfe885d20d6bccccd588ba25fa72
Pika <15848969+ThatNerdyPikachu@users.noreply.github.com>
2018-02-24 16:00:23 -0500

    bumps version

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='p!', description="""a fun loving pikachu who turned into a robot.
-its master is thatnerdypikachu. send help and ðŸ’™'s""", owner_id=124972333415792640)
+bot = commands.Bot(command_prefix='p!', description="""A multi-purpose Discord Bot created by ThatNerdyPikachu""", owner_id=124972333415792640)
 
 @bot.event
 async def on_ready():
@@ -12,7 +11,7 @@ async def on_ready():
     bot.load_extension("cogs.factions")
     bot.load_extension("cogs.moderation")
     bot.load_extension("cogs.starboard")
-    await bot.change_presence(game=discord.Game(name="with thunderbolts | v1.0 RC2"))
+    await bot.change_presence(game=discord.Game(name="with thunderbolts | v1.0"))
     print("ready")
 
 bot.run("your token")
