55ad6403e07266043a8f1bd24c9ce33e20a9986f
Pika <15848969+ThatNerdyPikachu@users.noreply.github.com>
2018-02-24 18:06:57 -0500

    resolves githubs "conflict", i think?

--- bot.py
+++ bot.py
@@ -15,10 +15,4 @@ async def on_ready():
     await bot.change_presence(game=discord.Game(name="with thunderbolts | v1.0 RC2"))
     print("ready")
     
-@bot.command()
-async def about(ctx):
-    embed = discord.Embed(color=0xffff00, title="About", description="**Pika.py** v1.0 RC2\n\nCreated with love by ThatNerdyPikachu and friends, including:\nthe me_irl discord server\n2Hats\nMaster9000\nKronos\nMaki (for the name)\nFrederikTheDane")
-    embed.set_footer(text="Thanks for checking me out! <3")
-    await ctx.send(embed=embed)
-
 bot.run("your token")
