32e65b6528fbbfbe053094066d115e8e81952fa6
Pika <15848969+ThatNerdyPikachu@users.noreply.github.com>
2018-03-08 10:32:55 -0500

    bump version to v1.1

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='p!', description="""A multi-purpose Discord Bot created by ThatNerdyPikachu""", owner_id=124972333415792640)
+bot = commands.Bot(command_prefix='p!', description="""A multi-purpose Discord Bot created by ThatNerdyPikachu""", owner_id=your id)
 
 @bot.event
 async def on_ready():
@@ -11,12 +11,12 @@ async def on_ready():
     bot.load_extension("cogs.factions")
     bot.load_extension("cogs.moderation")
     bot.load_extension("cogs.starboard")
-    await bot.change_presence(game=discord.Game(name="with thunderbolts | v1.0"))
+    await bot.change_presence(game=discord.Game(name="with pikas heartstrings ;-; | v1.1"))
     print("ready")
     
 @bot.command()
 async def about(ctx):
-    embed = discord.Embed(color=0xffff00, title="About", description="**Pika.py** v1.0\n\nCreated with love by ThatNerdyPikachu and friends, including:\nthe me_irl discord server\n2Hats\nMaster9000\nKronos\nMaki (for the name)\nFrederikTheDane")
+    embed = discord.Embed(color=0xffff00, title="About", description="**Pika.py** v1.1\n\nCreated with love by ThatNerdyPikachu and friends, including:\nthe me_irl discord server\n2Hats\nMaster9000\nKronos\nMaki (for the name)\nFrederikTheDane")
     embed.set_footer(text="Thanks for checking me out! <3")
     await ctx.send(embed=embed)
 bot.run("your token")
